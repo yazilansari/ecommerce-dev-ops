1b281281222a8d2cffd1c003291dd8ee60b3c184
yazilansari <yazilansari03@gmail.com>
2025-08-05 18:18:42 +0400

    Changed nextjs-app from NodePort to ClusterIP and changed Host in Ingress

--- nextjs-app/next.config.mjs
+++ nextjs-app/next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_PRODUCTS_API_URL: 'http://api.192-168-49-2.nip.io/products',
-    NEXT_PUBLIC_ORDERS_API_URL: 'http://api.192-168-49-2.nip.io/orders',
-    NEXT_PUBLIC_PAYMENT_API_URL: 'http://api.192-168-49-2.nip.io/payment',
-    NEXT_PUBLIC_AUTH_API_URL: 'http://api.192-168-49-2.nip.io/auth'
+    NEXT_PUBLIC_PRODUCTS_API_URL: 'http://api.172-18-0-3.nip.io/products',
+    NEXT_PUBLIC_ORDERS_API_URL: 'http://api.172-18-0-3.nip.io/orders',
+    NEXT_PUBLIC_PAYMENT_API_URL: 'http://api.172-18-0-3.nip.io/payment',
+    NEXT_PUBLIC_AUTH_API_URL: 'http://api.172-18-0-3.nip.io/auth'
   }
 };
 
