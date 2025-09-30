9e05fb0b84cde78638ca7259b32259acc216b24c
yazilansari <yazilansari03@gmail.com>
2025-09-30 11:47:53 +0400

    Changed Order File

--- orders-service/index.js
+++ orders-service/index.js
@@ -23,4 +23,4 @@ app.post('/orders', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Orderss Service Running on Port 3000'));
+app.listen(3000, () => console.log('Orders Service Running on Port 3000'));
